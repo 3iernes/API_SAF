cabc6279df0bb6c675fcb02885567a3bc1f92cc6
Federico Acosta <fede-2800@hotmail.com>
2023-08-15 15:33:20 -0300

    Cansado ya x2

--- src/controllers/camionesController.ts
+++ src/controllers/camionesController.ts
@@ -25,6 +25,7 @@ export const getResumenDespachos2 = async (req: Request, res:Response) =>{
 export const getToneladasDelSaf = async(req: Request, res:Response) => {
     try {
         const {ctrl_op_saf} = req.query
+        console.log(ctrl_op_saf)
         if(!ctrl_op_saf) return res.status(400).json({msj:'Falta el numero de operativo.'})
         //console.log(ctrl_op_saf)
         const db_result = await updateTnsEnLog(+ctrl_op_saf)
@@ -33,9 +34,9 @@ export const getToneladasDelSaf = async(req: Request, res:Response) => {
             return res.status(200).json({msj:'Ejecutado con exito, revise las toneladas y cantidad de viajes por las dudas.'})
         }else{
             return res.status(400).json({msj:`Hay ${db_result.dif_cam_sl} equipos del SAF que no se corresponden con equipos de Logistica.
-            Hay ${db_result.dif_cam_ls} equipos de Logistica que no se corresponden con equipos del SAF.
-            Recuerde que la accion solo se aplica con operativos finalizados en el saf.
-            Revise los datos.`})
+Hay ${db_result.dif_cam_ls} equipos de Logistica que no se corresponden con equipos del SAF.
+Recuerde que la accion solo se aplica con operativos finalizados en el saf.
+Revise los datos.`})
         }
     } catch (error) {
         console.log(error)
